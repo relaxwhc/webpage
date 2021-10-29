ad65d3dc95012c8173ae401154016254f7dad0da
mansana <relaxwhc@gmail.com>
2021-10-29 09:19:22 +0800

    webpage completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,17 @@
 // Tic Tac Toe JavaScript
 
 /*
-Click function playClick()
-Conditions to win winningCriteria()
-Announce the winner winningMessage()
-Reset the game gameReset()
+Click function to mark X or O on the grid: playClick()
+Conditions to win: winningCriteria()
+Announce the winner (X or O): winningMessage()
+Reset the game: gameReset()
 */
 
-// Onclick to activate symbol change:
 let gameStatus = "running";
 let counter = 1;
+
+// Onclick to activate symbol change (X or O):
+// odd counter number = X, even = O;
 function playClick(self) {
   if (self.innerHTML == "" && gameStatus == "running") {
     if (counter % 2 != 0) {
@@ -215,6 +217,10 @@ function winningCriteria() {
     document.getElementById("r3c3").style.backgroundColor = "gainsboro";
     winningMessage();
   }
+  // Default playing mode:
+  else {
+    gameStatus = "running";
+  }
 }
 
 // Winner Announcement
@@ -251,13 +257,13 @@ function gameReset() {
   document.getElementById("r3c2").innerHTML = "";
   document.getElementById("r3c3").innerHTML = "";
   // Clear background color
-  document.getElementById("r1c1").style.backgroundColor = "";
-  document.getElementById("r1c2").style.backgroundColor = "";
-  document.getElementById("r1c3").style.backgroundColor = "";
-  document.getElementById("r2c1").style.backgroundColor = "";
-  document.getElementById("r2c2").style.backgroundColor = "";
-  document.getElementById("r2c3").style.backgroundColor = "";
-  document.getElementById("r3c1").style.backgroundColor = "";
-  document.getElementById("r3c2").style.backgroundColor = "";
-  document.getElementById("r3c3").style.backgroundColor = "";
+  document.getElementById("r1c1").style.backgroundColor = "transparent";
+  document.getElementById("r1c2").style.backgroundColor = "transparent";
+  document.getElementById("r1c3").style.backgroundColor = "transparent";
+  document.getElementById("r2c1").style.backgroundColor = "transparent";
+  document.getElementById("r2c2").style.backgroundColor = "transparent";
+  document.getElementById("r2c3").style.backgroundColor = "transparent";
+  document.getElementById("r3c1").style.backgroundColor = "transparent";
+  document.getElementById("r3c2").style.backgroundColor = "transparent";
+  document.getElementById("r3c3").style.backgroundColor = "transparent";
 }
